165fa0515924ad9bc08e2fcdd5f1cfcfc3c0ee9f
MVFalcao <42094176+MVFalcao@users.noreply.github.com>
2018-08-20 14:40:33 -0300

    Update gameplay.h

--- Domino's Pizza/LibList/gameplay.h
+++ Domino's Pizza/LibList/gameplay.h
@@ -78,23 +78,20 @@ int Checkwin(tp_Piece *p1, tp_Piece *p2, tp_listade *jogo ,tp_stack *baralho){
     } else if(stack_empty(baralho)) { //verifica se o baralho esta vazio 
         int valor_r, valor_l,count = 0;
         if(search_Piece(p1, jogo->ini->v_L, jogo->ini->v_R) == 0){ 
-		conut++;
-		return 0};
+		count++;
 	if(search_Piece(p1, jogo->fim->v_L, jogo->fim->v_R) == 0){ 
 		count++;
-		return 0};//verifica os lados do jogo 
+	}//verifica os lados do jogo 
 	    if(count==2){
 		    count=0;
-		    passTurn();}
+		    passTurn();
+	    }
         if(search_Piece(p2, jogo->ini->v_L, jogo->ini->v_R) == 0){ 
-		count++;
-		return 0};//se existir chances de jogar//retorna 0 e continua o jogo
+		count++;}//se existir chances de jogar//retorna 0 e continua o jogo
         if(search_Piece(p2, jogo->fim->v_L, jogo->fim->v_R) == 0){ 
-		count++;
-		return 0};
-	      if(count==2){
-		    count=0;
-		    passTurn();}
+		count++;}
+	      if(count<2){
+		    return 0;}
         if(player_pecas_soma(p1) > player_pecas_soma(p2)) {
 		//soma as pecas que estao na mao de cada jogar se o jogador 1 tiver menos ele ganha se n o 2 ganha.
             return 1;
